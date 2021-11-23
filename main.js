4a98ac7b6021fee595f4b8db73e66f52ab4404c2
britomaximatech <lucassb90gmail.com>
2021-11-23 15:33:20 -0300

    mensage

---
+++ main.js
@@ -0,0 +1,143 @@
+/* - modelo de alerta popo up na tela
+
+var nome=" jaspion";
+var idade ="54";
+
+alert (nome+ " tem "+idade+" anos");
+
+-------
+
+var nota1 =1;
+var nota2 =35;
+var frase=" arroz, feijão, bife, bata frita"
+
+//o comando .replace na frente da sting sustitui o primeiro conteudo pelo segundo
+
+console.log (nota1+nota2);
+console.log (frase.replace("arroz", "salada"));
+alert(frase.toLocaleUpperCase());
+*/
+
+//--------
+
+//var listacomp=["roda","motor","janela"];
+//console.log(listacomp);
+//console.log (listacomp[1]) /*[1]posicao da string que sera mostrada */
+//console.log(listacomp.toString()); /*o comando tostring vai retornar somente as strings*/
+//console.log(listacomp.join(" & ")); /*  join usado para separar as strings */
+
+//var fruto = {nome:"cajamanga", cor: "amarila", formato: "arredondado"} /* {}- dicionario, []-lista */
+
+//console.log(fruto); /* mostra as informações da string entre chaves */
+//alert(fruto.formato);
+
+//----variaveis
+
+//var idade=prompt("qual tua idade?"); /* o browser abre  para colocar a varialvel */
+//if (idade >=22){
+ //  alert("maior de idade")
+//} else{
+ //  alert("menor de idade")
+//};
+
+
+
+/*var valor = prompt ("valor da compra");
+if (valor >=100){
+    alert ("desconto de 20%")
+}  else {
+    alert("desconto de 10")
+}*/
+
+
+//variaveis de repetição
+/*var count=0;
+while (count<=5){
+    console.log(count);
+    count=count +1;
+}
+
+
+var count;
+for(count=0; count <=5; count++){
+    alert (count);
+};*/
+
+//date
+
+//var d = new Date()
+//alert(d.getMonth()+1);  /*getmonth ()+1 para trazer o mes correto */
+
+
+
+/* criar funções*/
+
+/*var validar=0;
+function validaid(idade){
+    
+    if (idade >=18){
+        validar=true
+    }else{
+        validar=false
+    }
+    return validar
+}
+var idade= prompt("qual a tua idade?");
+validaid(idade)
+console.log(validar);*/
+
+/*functions link cada no html */
+
+
+
+//function aperte(){
+//    document.getElementById("thanks").innerHTML ="obrigado peoples";
+//   console.log(document.getElementById("thanks"));
+//}
+
+//function redirection(){
+//    window.open("https://digitalinnovation.one/");                     /*abrir em outra aba*/
+//    window.location.href="https://digitalinnovation.one/"             /*abrir na mesma aba*/
+//}
+
+//function trocar(){
+//  document.getElementById("rato").innerHTML =" obrigado por passar o mouse";  /*subistituir o texto do html*/ 
+ // alert("trocar texto");
+//}
+
+//function voltar() {
+//    document.getElementById("rato").innerHTML ="passe o rato aqui";  
+//}
+
+//function load(){
+//    alert ("page carregada");
+//}
+
+//function funchange(elemento){
+//    console.log(elemento.value);
+//} 
+
+/*
+
+
+let valor1 = parseInt(gets(25));
+
+
+
+let valor2 = parseInt(gets(100));
+
+
+
+let valor3 = parseFloat(gets(5.50));
+
+
+
+let salary = parseFloat(valor2 * valor3).toFixed(2);
+
+
+
+console.log("NUMBER = " + valor1);
+
+
+
+console.log("SALARY = U$ " + salary); */
